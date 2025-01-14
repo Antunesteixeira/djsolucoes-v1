69025a53dcbd0c9e7d0e1a9c3292b0680d072941
Antunesteixeira <antunesteixeira@outlook.com>
2025-01-14 16:00:16 -0300

    correção de cpf colaborador

--- colaborador/models.py
+++ colaborador/models.py
@@ -7,7 +7,7 @@ class Colaborador(models.Model):
     nomesocialCol = models.CharField(max_length=255, null=True)
     usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     telefone = models.CharField(max_length=11)
-    cpf_cnpj = models.CharField(max_length=20, null=True, unique=True)
+    cpf_cnpj = models.CharField(max_length=20, null=True)
     pix = models.CharField(max_length=100)
     banco = models.CharField(max_length=100, null=True)
     funcao = models.CharField(max_length=100)
